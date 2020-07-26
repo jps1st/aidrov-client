cc33162e82a93301b1e7ae6dcf736e594367324e
johnsolano <john.solano@formbird.com>
2020-07-26 21:13:26 +0800

    inital commit

---
+++ src/@vex/components/page-layout/page-layout-header.directive.ts
@@ -0,0 +1,15 @@
+import {Directive} from '@angular/core';
+
+@Directive({
+    selector: '[vexPageLayoutHeader],vex-page-layout-header',
+    host: {
+        class: 'vex-page-layout-header'
+    }
+})
+export class PageLayoutHeaderDirective {
+
+    constructor() {
+    }
+
+}
+
---
+++ src/app/pages/apps/help-center/help-center-faq/help-center-faq.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'vex-help-center-faq',
+    templateUrl: './help-center-faq.component.html',
+    styleUrls: ['./help-center-faq.component.scss']
+})
+export class HelpCenterFaqComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
